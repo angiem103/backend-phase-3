d49ae32d3bfd77fd47cbe0ee3386aec9e513c66c
angie <amaticorena103@hotmail.com>
2022-05-17 21:12:24 -0400

    creating tables

--- db/migrate/20220518005725_create_veterinarians.rb
+++ db/migrate/20220518005725_create_veterinarians.rb
@@ -1,4 +1,8 @@
 class CreateVeterinarians < ActiveRecord::Migration[6.1]
   def change
+    create_table :veterinarians do |t|
+      t.string :name
+      t.string :phone_number
+    end
   end
 end
